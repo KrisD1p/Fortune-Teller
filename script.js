9234f51cc54f861fc6bbd85006010a4002ff0a89
KrisD1p <43444253+KrisD1p@users.noreply.github.com>
2019-03-26 09:33:21 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$("button").click(function(){
+    var name = $(".name").val();
+    console.log(name);
+    
+    var born = $(".born").val();
+    console.log(born + 20);
+     
+    var sport = $(".sport").val();
+    console.log(sport);
+    
+});
